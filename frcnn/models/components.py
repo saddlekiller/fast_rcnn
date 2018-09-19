52a3a4c2d45880a0a77bd49d4a0f9cb4c0fbbf63
cheng <alan_debug@icloud.com>
2018-09-19 23:12:21 +0800

    complete first version VGG

--- frcnn/models/components.py
+++ frcnn/models/components.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 from frcnn.models.modules import *
 from frcnn.utils.utils import *
+from PIL import Image
 
 class SPP():
 
@@ -32,9 +33,13 @@ class VGG16():
     def __init__(self):
         self.stack_filters = [3, 64, 128, 256, 512, 512]
         self.stack_denses = [512, 4096, 4096, 1000]
-        self.layers = []
+        self.layers = {}
+        self.global_step = tf.get_variable(name='global_step', shape=(), dtype=tf.int64)
+
         with tf.variable_scope('vgg_16'):
+            self.mean_rgb = tf.get_variable(name='mean_rgb', shape=(3), dtype=tf.float32)
             for i in range(len(self.stack_filters) - 1):
+                self.layers['stack_conv_{}'.format(i+1)] = {}
                 with tf.variable_scope('conv{}'.format(i+1)):
                     if i == 0 or i == 1:
                         n_layers = 2
@@ -43,27 +48,38 @@ class VGG16():
                     for j in range(n_layers):
                         with tf.variable_scope('conv{}_{}'.format(i+1, j+1)):
                             if j == 0:
-                                self.layers.append(Conv2D3x3(in_channels=self.stack_filters[i],
-                                                            out_channels=self.stack_filters[i+1]))
+                                self.layers['stack_conv_{}'.format(i+1)]['conv_{}'.format(j+1)] = Conv2D3x3(in_channels=self.stack_filters[i],
+                                                            out_channels=self.stack_filters[i+1])
                             else:
-                                self.layers.append(Conv2D3x3(in_channels=self.stack_filters[i+1],
-                                                            out_channels=self.stack_filters[i+1]))
+                                self.layers['stack_conv_{}'.format(i + 1)]['conv_{}'.format(j + 1)] = Conv2D3x3(in_channels=self.stack_filters[i+1],
+                                                            out_channels=self.stack_filters[i+1])
+                            self.layers['stack_conv_{}'.format(i+1)]['relu_{}'.format(j + 1)] = ReluActivation()
+                    self.layers['stack_conv_{}'.format(i+1)]['maxpooling'] = MaxPooling2D(pool_size=(2, 2),
+                                                                                          strides=(2, 2),
+                                                                                          padding='VALID')
+
+            self.layers['stack_dense'] = {}
             with tf.variable_scope('fc6'):
-                self.layers.append(Conv2D7x7(in_channels=512,
-                                             out_channels=4096))
+                self.layers['stack_dense']['dense_6'] = Conv2D7x7(in_channels=512,
+                                             out_channels=4096, padding='VALID')
             with tf.variable_scope('fc7'):
-                self.layers.append(Conv2D1x1(in_channels=4096,
-                                             out_channels=4096))
+                self.layers['stack_dense']['dense_7'] = Conv2D1x1(in_channels=4096,
+                                             out_channels=4096)
             with tf.variable_scope('fc8'):
-                self.layers.append(Conv2D1x1(in_channels=4096,
-                                             out_channels=1000))
+                self.layers['stack_dense']['dense_8'] = Conv2D1x1(in_channels=4096,
+                                             out_channels=1000)
 
     def load(self, ckpt_path, sess):
         saver = tf.train.Saver()
         saver.restore(sess, ckpt_path)
 
     def __call__(self, inputs):
-        x = inputs
+        x = inputs - self.mean_rgb / 255
+        for stack_name, stack_layers in self.layers.items():
+            for layer_name, layer in stack_layers.items():
+                x = layer(x)
+        return tf.nn.top_k(tf.squeeze(x, [2, 2]), k = 3)[1]
+
 
 class ResNet():
 
@@ -75,9 +91,17 @@ class ResNet():
 
 
 if __name__ == '__main__':
+    # print_model('../pretrain/vgg_16.ckpt', all_tensors=True)
+    img = Image.open('../tests/images/timg.jpeg')
+    img = img.resize((224, 224))
+    img = np.array(img) / 255
+    img = np.expand_dims(img, 0)
+    print(np.array(img).shape)
 
     with tf.Session() as sess:
         vgg = VGG16()
         vgg.load('../pretrain/vgg_16.ckpt', sess)
-
-
+        results = sess.run(vgg(tf.constant(img, dtype=tf.float32)))[0]
+        print(results[0])
+        # print(sess.run(vgg.mean_rgb))
+        # print(sess.run(vgg.global_step))
--- frcnn/models/modules.py
+++ frcnn/models/modules.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 class Conv2D():
 
-    def __init__(self, ksize, in_channels, out_channels, use_bias=True):
+    def __init__(self, ksize, in_channels, out_channels, padding='SAME', use_bias=True):
         if type(ksize) == int:
             weight_shape = (ksize, ksize, in_channels, out_channels)
         else:
@@ -12,16 +12,17 @@ class Conv2D():
         self.weights = tf.get_variable(name='weights',
                                        initializer=tf.truncated_normal_initializer(),
                                        shape=weight_shape)
+        self.padding = padding
         if self.use_bias:
             self.biases = tf.get_variable(name='biases',
                                           initializer=tf.zeros_initializer(),
                                           shape=(out_channels))
 
-    def __call__(self, inputs, strides=(1, 1, 1, 1), padding='SAME'):
+    def __call__(self, inputs, strides=(1, 1, 1, 1)):
         outputs = tf.nn.conv2d(input=inputs,
                                filter=self.weights,
                                strides=strides,
-                               padding=padding)
+                               padding=self.padding)
         if self.use_bias:
             outputs = tf.nn.bias_add(outputs, self.biases)
         return outputs
@@ -29,18 +30,18 @@ class Conv2D():
 
 class Conv2D3x3(Conv2D):
 
-    def __init__(self, in_channels, out_channels, use_bias=True):
-        super(Conv2D3x3, self).__init__(3, in_channels, out_channels, use_bias)
+    def __init__(self, in_channels, out_channels, padding='SAME', use_bias=True):
+        super(Conv2D3x3, self).__init__(3, in_channels, out_channels, padding, use_bias)
 
 class Conv2D7x7(Conv2D):
 
-    def __init__(self, in_channels, out_channels, use_bias=True):
-        super(Conv2D7x7, self).__init__(7, in_channels, out_channels, use_bias)
+    def __init__(self, in_channels, out_channels, padding='SAME', use_bias=True):
+        super(Conv2D7x7, self).__init__(7, in_channels, out_channels, padding, use_bias)
 
 class Conv2D1x1(Conv2D):
 
-    def __init__(self, in_channels, out_channels, use_bias=True):
-        super(Conv2D1x1, self).__init__(1, in_channels, out_channels, use_bias)
+    def __init__(self, in_channels, out_channels, padding='SAME', use_bias=True):
+        super(Conv2D1x1, self).__init__(1, in_channels, out_channels, padding, use_bias)
 
 class Dense():
 
@@ -59,3 +60,19 @@ class Dense():
         if self.use_bias:
             outputs = tf.nn.bias_add(outputs, self.biases)
         return outputs
+
+class MaxPooling2D():
+
+    def __init__(self, pool_size, strides, padding):
+        self.pooling = tf.layers.MaxPooling2D(pool_size=pool_size, strides=strides, padding=padding)
+
+    def __call__(self, inputs):
+        return self.pooling(inputs)
+
+class ReluActivation():
+
+    def __init__(self):
+        pass
+
+    def __call__(self, inputs):
+        return tf.nn.relu(inputs)
